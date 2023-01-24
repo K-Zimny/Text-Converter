5b10461037009d6ac6dbac1d05e74cd672803036
Kenny Zimny <kenny.zimny@criticalmass.com>
2023-01-24 15:08:55 -0600

    feature/created text transform function

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 submitBtn = document.getElementById("submitDisplay");
 textBox = document.getElementById("textBoxDisplay");
+resultDisplay = document.getElementById("resultDisplay")
 
+//Transform upcase text to lowercase. Cap first Letter
 submitBtn.addEventListener("click", function(){
-    let textSubmission = textBox.value;
-    console.log(textSubmission)
+    let textSubmission = textBox.value.toLowerCase();
+    textSubmission = textSubmission.split("")
+    textSubmission[0] = textSubmission[0].toUpperCase();
+    textSubmission = textSubmission.join("");
+    resultDisplay.innerHTML = textSubmission
 })
