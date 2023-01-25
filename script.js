72a283346e624652e2c425034cb5a49e217017a8
Kenny Zimny <kenny.zimny@criticalmass.com>
2023-01-25 10:40:44 -0600

    Improved user experience

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 submitBtn = document.getElementById("submitDisplay");
 textBox = document.getElementById("textBoxDisplay");
-resultDisplay = document.getElementById("resultDisplay")
+resultDisplay = document.getElementById("resultDisplay");
+let copyBtn = document.getElementById("copyButton");
 
 let textSubmission; 
 
 //Transform upcase text to lowercase. Cap first Letter
 submitBtn.addEventListener("click", function(){
     textSubmission = textBox.value.toLowerCase();
-    textSubmission = textSubmission.split("")
+    textSubmission = textSubmission.split("");
     textSubmission[0] = textSubmission[0].toUpperCase();
     textSubmission = textSubmission.join("");
-    resultDisplay.innerHTML = textSubmission
+    resultDisplay.innerHTML = textSubmission;
 })
 
 //Cap the first letter of each sentence
 submitBtn.addEventListener("click", function(){
-    textSubmission = textSubmission.split(". ")
+    textSubmission = textSubmission.split(". ");
     for (let index = 0; index < textSubmission.length; index++) {
         textSubmission[index] = textSubmission[index].charAt(0).toUpperCase() + textSubmission[index].slice(1);
     }
-    textSubmission = textSubmission.join(". ")
-    resultDisplay.innerHTML = textSubmission
+    textSubmission = textSubmission.join(". ");
+    resultDisplay.innerHTML = textSubmission;
 })
+
